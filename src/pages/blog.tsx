f2effe76a1a83a2bb84a250bdeb5e8777ae1c7cf
jeong922 <hwajeong922@naver.com>
2023-09-19 16:38:47 +0900

    Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import { graphql, PageProps } from 'gatsby';
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTilteQuery>) {
+  console.log(data);
   return (
     <Layout title='Blog'>
-      <p>The most recent news from my shop</p>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTilte {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title='Blog' />;

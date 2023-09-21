9640995fa32a2443606e5f2d44773bead4f653c5
jeong922 <hwajeong922@naver.com>
2023-09-21 16:41:49 +0900

    Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import { graphql } from 'gatsby';
+import Seo from '../../components/Seo';
 
-export default function BlogPost() {
+type Props = {
+  data: Queries.PostDetailQuery;
+  children: React.ReactNode;
+};
+
+export default function BlogPost({ data, children }: Props) {
+  console.log(children);
   return (
-    <Layout title='Blog Post'>
-      <div></div>
+    <Layout title=''>
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        title
+        data
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: Props) => (
+  <Seo title={data.mdx?.frontmatter?.title!} />
+);

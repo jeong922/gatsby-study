d56a504d2973243f825df2bbf4d7b2bb20d0ebfc
jeong922 <hwajeong922@naver.com>
2023-09-22 19:45:48 +0900

    StaticImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function IndexPage() {
   return (
     <Layout title='Welcome to DevStickers ❤'>
-      <div></div>
+      <StaticImage
+        height={500}
+        src='https://images.unsplash.com/photo-1531306728370-e2ebd9d7bb99?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80'
+        alt='image'
+      />
     </Layout>
   );
 }
